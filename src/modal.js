aff47a328c02f4af8f07de033f0dc3cda95a105b
aaaaus <ausluft@gmail.com>
2019-06-24 18:12:13 -0400

    further modal work

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('modal');
+  const modalClose = document.getElementById('modal-close');
+
+  const modalDescription = document.getElementById('modal-description');
 
   //this returns an HTMLCollection, not a NodeList - cannot use forEach on this
   //const projectCards = document.getElementsByClassName('box');
@@ -7,12 +10,33 @@ document.addEventListener('DOMContentLoaded', () => {
   //this returns a NodeList
   const projectCards = document.querySelectorAll('button.project-button');
 
+  let currentProject = {};
+
+  //helper to set project on click
+  function setCurrentProject(name) {
+    projects.forEach(project => {
+      if (project.name === name) {
+        currentProject = project;
+      }
+    });
+  }
+
+  //helper to change modal inner HTML
+  function renderModalContent(project) {
+    console.log(`I am going to change the content to ${project.name} content`);
+    const { name, year, technologies, description } = project;
+    modalDescription.innerHTML = `${description}`;
+  }
+
   projectCards.forEach(projectCard => {
     projectCard.addEventListener('click', () => {
-      console.log('Clicked a project');
       modal.style.display = 'block';
+      setCurrentProject(event.target.id);
+      renderModalContent(currentProject);
     });
   });
 
-  console.log(projectCards);
+  modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
 });
---
+++ src/projectData.js
@@ -0,0 +1,8 @@
+const projects = [
+  {
+    name: 'PropDash',
+    year: '2019',
+    technologies: 'React/Redux, Rails',
+    description: 'PropDash is an app that does proptech'
+  }
+];
